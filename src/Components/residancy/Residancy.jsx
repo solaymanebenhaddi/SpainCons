917517d33670896067f663114c33b5dc52cff003
solaymanebenhaddi <heddoudi.mohamed@gmail.com>
2022-07-15 11:25:09 +0000

    add import commit

--- src/Components/residancy/Residancy.jsx
+++ src/Components/residancy/Residancy.jsx
@@ -1,3 +1,5 @@
+import { faDollar, faGlobe, faHandHoldingDollar, faHourglass, faPassport, faPeopleRoof} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import pass from "./pass.png"
 
@@ -28,6 +30,46 @@ function Residancy() {
             <img src={pass} alt="" srcset="" className='img-fluid' />
             
           </div>
+          <div className="col-md-6 d-flex align-items-start">
+            <ul className='list float-start'>
+              <li className='d-flex align-items-center p-0'>
+                <span ><FontAwesomeIcon icon={faHourglass} className='fs-3 text-light mx-3'/></span>
+                <div><h6>Time to citizenship</h6></div>
+                <div><p>Visa-free to 29 Schengen countries</p></div>
+              </li>
+              <li className='d-flex justify-content-between align-items-center p-0'>
+                <span ><FontAwesomeIcon icon={faGlobe} className='fs-3 text-light mx-3'/></span>
+                <div><h6>Visa free travel</h6></div>
+                <div><p>10 years (and must reside in Spain)</p></div>
+              </li>
+              <li className='d-flex justify-content-between align-items-center p-0'>
+                <span ><FontAwesomeIcon icon={faHandHoldingDollar} className='fs-3 text-light mx-3'/></span>
+                <div><h6>Investment type</h6></div>
+                <div><p>Real Estate</p></div>
+              </li>
+              <li className='d-flex justify-content-between align-items-center p-0'>
+                <span ><FontAwesomeIcon icon={faDollar} className='fs-3 text-light mx-3'/></span>
+                <div><h6>Investment cost</h6></div>
+                <div><p>EUR 500,000</p></div>
+              </li>
+              <li className='d-flex justify-content-between align-items-center p-0'>
+                <span ><FontAwesomeIcon icon={faPeopleRoof} className='fs-3 text-light mx-3'/></span>
+                <div><h6>Family dependents</h6></div>
+                <div><p>Spouse and children below 18. Adult children above 18, and parents of main applicant qualify as long as they prove financial dependency</p></div>
+              </li>
+              <li className='d-flex justify-content-between align-items-center p-0'>
+                <span ><FontAwesomeIcon icon={faPassport} className='fs-3 text-light mx-3'/></span>
+                <div><h6>Passport validity</h6></div>
+                <div><p>10 years (and must reside in Spain)</p></div>
+              </li>
+            </ul>
+           
+          </div>
+          <div className="col-md-6"> <div className='section-title'><p className='text-dark'>The The Spanish “Golden Visa” with AREC SPAIN</p></div>
+
+            <p className='text-light'>
+            The Spanish “Golden Visa” Law was introduced in 2013, allowing non-EU citizens and members of their families the opportunity to get a Temporary Residence Permit. The Temporary Resident Permits will also be granted to Spouses and any dependent children (under 18 years of age) but will not give the resident benefits such as free healthcare and education or other social benefits and so the investor and his or her family must have health insurance and be able to show sufficient financial resources to support themselves.
+            </p></div>
 
         </div>
 
